07f73326d3b8e14ba4f781608e904400d5a5589d
yegor <yegorkhohlov@gmail.com>
2025-01-29 23:23:17 +0200

    add build options

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,21 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("org.example.MainKt")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "org.example.MainKt"
+    }
+}
 
 repositories {
     mavenCentral()
