6470482fd61dfd389aaee725ce991ac0315e6090
yegor <yegorkhohlov@gmail.com>
2025-01-29 23:58:04 +0200

    add subscribe/unsubscribe commands

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,9 @@ import com.github.kotlintelegrambot.dispatcher.command
 import com.github.kotlintelegrambot.entities.ChatId
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
+import java.util.*
 
+val subscribedChats = mutableSetOf<Long>()
 
 fun daysUntilWarEnd(): Long {
     val targetDate = LocalDate.of(2025, 5, 2)
@@ -25,8 +27,19 @@ fun generateProgressBar(barWidth: Int = 10): String {
     return "$days days left until the war end\n $filled$empty $safePercentage%"
 }
 
+fun getNext10amDate(): Date = Calendar.getInstance().apply {
+    set(Calendar.HOUR_OF_DAY, 10)
+    set(Calendar.MINUTE, 0)
+    set(Calendar.SECOND, 0)
+    set(Calendar.MILLISECOND, 0)
+
+    if (time.before(Date())) {
+        add(Calendar.DAY_OF_MONTH, 1)
+    }
+}.time
+
 fun main() {
-    bot {
+    val bot = bot {
         token = "7892383164:AAEaGEfKiUw0PbJP1oJV7LWRKRlpsHD3lWs"
         dispatch {
             command("start") {
@@ -42,6 +55,38 @@ fun main() {
                     text = generateProgressBar()
                 )
             }
+
+            command("subscribe") {
+                subscribedChats.add(message.chat.id)
+                bot.sendMessage(
+                    chatId = ChatId.fromId(message.chat.id),
+                    text = "You have been subscribed to progress updates"
+                )
+            }
+
+            command("unsubscribe") {
+                subscribedChats.add(message.chat.id)
+                bot.sendMessage(
+                    chatId = ChatId.fromId(message.chat.id),
+                    text = "You have been unsubscribed from progress updates"
+                )
+            }
         }
-    }.startPolling()
+    }
+    bot.startPolling()
+
+    Timer().scheduleAtFixedRate(
+        object : TimerTask() {
+            override fun run() {
+                subscribedChats.forEach { chatId ->
+                    bot.sendMessage(
+                        chatId = ChatId.fromId(chatId),
+                        text = generateProgressBar()
+                    )
+                }
+            }
+        },
+        getNext10amDate(),
+        24 * 60 * 60 * 1000L
+    )
 }
